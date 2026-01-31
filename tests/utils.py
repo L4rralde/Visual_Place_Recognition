d4658537ec90e0c2aefa1d41a8945aebc1b4b1f7
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2026-01-31 14:03:48 -0600

    Detaching da3 and skipping attention instructions that are not used for salad training. Aiming to reduce vram memory

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,9 @@
 import os, sys
-import random
-import numpy as np
+from typing import List, Callable
+import glob
+
+from PIL import Image
+from torch.data.utils import Dataset
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import vpr.models.backbones.da3.da3 as da3
@@ -22,7 +25,7 @@ def load_da3_as_is(config_name: str='BASE'):
 def load_da3_dino(config_name: str='BASE', return_token: bool=True):
     if not config_name in supported_configs:
         raise ValueError(f"Configuration {config_name} is not supported. Try one of the followings: {supported_configs}")
-    da3_dino = da3.DepthAnything3Dino(
+    da3_dino = da3.DepthAnything3Dino.from_pretrained(
         model_name=f'da3-{config_name.lower()}',
         return_token=return_token,
     )
@@ -36,3 +39,28 @@ def freeze_model(model) -> None:
         param.requires_grad = False
     model.eval()
 
+
+class ImgDirDataset(Dataset):
+    def __init__(self, img_dir: str, transform: Callable|None = None):        
+        self.img_pahts = ImgDirDataset.scan_dir(img_dir)
+        assert len(self.img_pahts) > 0, "Found no valid image"
+        self.transform = transform
+
+    @staticmethod
+    def scan_dir(img_dir: str) -> List[str]:
+        valid_exts = ['*.jpg', '*.jpeg', '*.png', '*.bmp']
+        all_img_paths = []
+        for ext in valid_exts:
+            all_img_paths.extend(glob.glob(os.path.join(img_dir, ext)))
+        
+        return all_img_paths
+
+    def __len__(self) -> int:
+        return len(self.img_pahts)
+
+    def __getitem__(self, index) -> Image.Image:
+        img_path = self.img_pahts[index]
+        img = Image.open(img_path)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, img_path
--- vpr/models/backbones/da3/__init__.py
+++ vpr/models/backbones/da3/__init__.py
@@ -1,2 +1,2 @@
-from .da3 import DepthAnything3Dino
+from .da3 import DepthAnything3Dino, da3_from_pretained
 from .transforms import get_transforms
