8942b8d982420a962bf1e17d4a64be16f6ad6a84
l4rralde <ealarralde@gmail.com>
2026-03-21 11:39:13 -0600

    Adding MapAnythingBackbone to comparison

--- tests/test_mapanything_salad_vs_mapanything_consistency_check.py
+++ tests/test_mapanything_salad_vs_mapanything_consistency_check.py
@@ -9,7 +9,7 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from vpr.models.backbones.mapanything import load_pretrained_mapanything, mapanything_inference
-from model_flavors.mapanything_salad import MapAnythingSalad
+from model_flavors.mapanything_salad import MapAnythingSalad, MapAnythingBackbone
 from test_utils import ImgDirDataset
 
 
@@ -22,9 +22,24 @@ def parse_args() -> dict:
     return args
 
 
+def compare_preds(baseline_preds, preds) -> float:
+    acc_diff = 0.0
+    for i, (ref_pred, pred) in enumerate(zip(baseline_preds, preds)):
+        for k in ref_pred.keys():
+            try:
+                diff = (pred[k] - ref_pred[k]).abs().sum()
+            except:
+                diff = (pred[k] ^ ref_pred[k]).to(torch.float32).sum()
+            if diff >= 1e-6:
+                raise RuntimeError(f"{k} mismatch: {diff} in view {i}")
+            acc_diff += diff
+    return acc_diff
+
+
 def compare_pipelines(
     baseline: torch.nn.Module,
     with_salad: MapAnythingSalad,
+    as_backbone: MapAnythingBackbone,
     dataset: Iterable,
     device: str,
     max_batch_size: int=10,
@@ -46,17 +61,17 @@ def compare_pipelines(
         baseline_preds = mapanything_inference(baseline, selected_paths)
         with_salad_preds = with_salad.inference(selected_paths)
 
-        acc_diff = 0.0
-        for i, (ref_pred, pred) in enumerate(zip(baseline_preds, with_salad_preds)):
-            for k in ref_pred.keys():
-                try:
-                    diff = (pred[k] - ref_pred[k]).abs().sum()
-                except:
-                    diff = (pred[k] ^ ref_pred[k]).to(torch.float32).sum()
-                if diff >= 1e-6:
-                   raise RuntimeError(f"{k} mismatch: {diff}")
-                acc_diff += diff
-        
+        try:
+            acc_diff = compare_preds(baseline_preds, with_salad_preds)
+        except RuntimeError as error:
+            raise RuntimeError(f"Salad preds mismatch: {error}")
+
+        asbackbone_preds = as_backbone.inference(selected_paths)
+        try:
+            acc_diff += compare_preds(baseline_preds, asbackbone_preds)
+        except RuntimeError as error:
+            raise RuntimeError(f"As backbone preds mismatch: {error}")
+
         status = "PASS" if acc_diff < 1e-6 else "FAIL"
         assert status == "PASS"
 
@@ -69,8 +84,9 @@ def main() -> None:
 
     print("Loading mapanything...")
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    mapanything = load_pretrained_mapanything().to(device)
+    mapanything = load_pretrained_mapanything().eval().to(device)
     print("Finished loading mapanything.")
+    as_backbone = MapAnythingBackbone(mapanything)
     agg_args = {
         'num_clusters': 64,
         'cluster_dim': 128,
@@ -78,7 +94,14 @@ def main() -> None:
     }
     with_salad = MapAnythingSalad(mapanything, agg_args=agg_args).to(device)
 
-    compare_pipelines(mapanything, with_salad, dataset, device, num_seeds=args.num_seeds)
+    compare_pipelines(
+        mapanything,
+        with_salad,
+        as_backbone,
+        dataset,
+        device,
+        num_seeds=args.num_seeds
+    )
 
 
 if __name__ == '__main__':
